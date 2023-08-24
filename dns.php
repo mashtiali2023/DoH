635991344eae12b6edbc781c5ef1fc66ad5a44cd
mashtiali2023 <126881422+mashtiali2023@users.noreply.github.com>
2023-08-24 10:55:41 +0330

    Update dns.php
    hamid-kord

--- dns.php
+++ dns.php
@@ -3,7 +3,7 @@ if (isset($_SERVER['CONTENT_TYPE']) && $_SERVER['CONTENT_TYPE'] == 'application/
 {
         $request = file_get_contents("php://input");
         header("Content-Type: application/dns-message");
-        $s = fsockopen("udp://127.0.0.1", 53, $errno, $errstr);
+        $s = fsockopen("udp://8.8.8.8", 53, $errno, $errstr);
         if ($s)
         {
                 fwrite($s, $request);
@@ -15,7 +15,7 @@ else if (isset($_GET['dns']))
 {
         $request = base64_decode(str_replace(array('-', '_'), array('+', '/'), $_GET['dns']));
         header("Content-Type: application/dns-message");
-        $s = fsockopen("udp://127.0.0.1", 53, $errno, $errstr);
+        $s = fsockopen("udp://8.8.8.8", 53, $errno, $errstr);
         if ($s)
         {
                 fwrite($s, $request);
